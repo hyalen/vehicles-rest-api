e7fd4ec9754412f2fb6e6b1a7cbe14756b70c43c
hyalen <hyalen.neves@toroinvestimentos.com.br>
2021-02-07 13:42:00 -0300

    Converted Pricing Service REST API to a microservice.

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -2,4 +2,5 @@ package com.udacity.pricing.domain.price;
 
 import org.springframework.data.repository.CrudRepository;
 
+// Interface that will perform CRUD operations from the Data Rest API specs
 public interface PriceRepository extends CrudRepository<Price, Long> {}
